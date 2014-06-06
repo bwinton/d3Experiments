dd4e5ff4e818c5e1c861784d23c8c8577ca43df2
Blake Winton <bwinton@latte.ca>
2014-06-06 15:43:41 -0400

    Use the customizationready event, and forward-button.disabled instead of the progress listener!  :)

--- heatmap.scratchpad.js
+++ heatmap.scratchpad.js
@@ -6,41 +6,6 @@ var elements = [
   'add-ons-button',
   'alltabs-button',
   'back-button',
-  'bookmarks-menu-button',
-  'characterencoding-button',
-  'copy-button',
-  'cut-button',
-  'developer-button',
-  'downloads-button',
-  'e10s-button',
-  'email-link-button',
-  'feed-button',
-  'find-button',
-  'forward-button',
-  'fullscreen-button',
-  'home-button',
-  'menu-button-button',
-  'new-tab-button',
-  'new-window-button',
-  'open-file-button',
-  'paste-button',
-  'preferences-button',
-  'print-button',
-  'privatebrowsing-button',
-  'save-page-button',
-  'sidebar-button',
-  'social-share-button',
-  'sync-button',
-  'tabsclose-button',
-  'tabview-button',
-  'urlbar-go-button',
-  'urlbar-reload-button',
-  'urlbar-stop-button',
-  'webrtc-status-button',
-  'zoom-in-button',
-  'zoom-out-button',
-  'zoom-reset-button',
-
   'BMB_bookmarksPopup',
   'BMB_bookmarksToolbarPopup',
   'BMB_unsortedBookmarksPopup',
@@ -48,62 +13,81 @@ var elements = [
   'bookmarks-bar-container',
   'bookmarks-bar-item',
   'bookmarks-bar-overflowed-item',
+  'bookmarks-menu-button',
+  'characterencoding-button',
+  'copy-button',
+  'cut-button',
+  'developer-button',
+  'downloads-button',
+  'e10s-button',
   'edit-controls',
+  'email-link-button',
+  'feed-button',
+  'find-button',
+  'forward-button',
+  'fullscreen-button',
   'history-panelmenu',
+  'home-button',
+  'menu-button-button',
   'menubar-items',
   'menubar-menu',
   'menubar-menuitem',
   'menubar-other',
   'nav-bar',
+  'new-tab-button',
+  'new-window-button',
+  'open-file-button',
   'PanelUI-contents',
   'PanelUI-menu-button',
+  'paste-button',
   'personal-bookmarks',
   'PersonalToolbar',
   'PlacesChevron',
   'PlacesToolbarItems',
-  'searchbar',
+  'preferences-button',
+  'print-button',
+  'privatebrowsing-button',
+  'save-page-button',
   'search-container',
+  'searchbar',
+  'sidebar-button',
+  'social-share-button',
+  'sync-button',
   'tabbrowser-tabs',
+  'tabsclose-button',
   'TabsToolbar',
+  'tabview-button',
   'toolbar-menubar',
   'urlbar-container',
-  'zoom-controls'
+  'urlbar-go-button',
+  'urlbar-reload-button',
+  'urlbar-stop-button',
+  'webrtc-status-button',
+  'zoom-controls',
+  'zoom-in-button',
+  'zoom-out-button',
+  'zoom-reset-button'
 ];
 
 // Get the browser into the right mode.
-const STATE_START = Ci.nsIWebProgressListener.STATE_START;
-const STATE_STOP = Ci.nsIWebProgressListener.STATE_STOP;
-var myListener = {
-    QueryInterface: XPCOMUtils.generateQI(['nsIWebProgressListener',
-                                           'nsISupportsWeakReference']),
-    onLocationChange: function(aProgress, aRequest, aURI) {
-      if (aURI.spec === 'about:customizing') {
-        setTimeout(function () {
-          window.content.location = 'about:config';
-        }, 1000);
-      } else if (aURI.spec === 'about:config') {
-        setTimeout(function () {
-          gBrowser.removeProgressListener(myListener);
-          window.content.back();
-        }, 1000);
+gNavToolbox.addEventListener("customizationready", function UI_loaded() {
+  document.getElementById('forward-button').disabled = false;
+  
+  setTimeout(function () {
+    var locs = 'id,x,y,width,height\n';
+
+    elements.forEach(i => {
+      let element = document.getElementById(i);
+      if (element) {
+        let bounds = element.getBoundingClientRect();
+        locs += i + ',' + bounds.x + ',' + bounds.y + ',' + bounds.width + ',' + bounds.height + '\n';
+      } else {
+        locs += i + ',-1,-1,0,0\n';
       }
-      return true;
-    }
-};
-
-// Get the browser into the right mode.
-gBrowser.addProgressListener(myListener);
-window.content.location = 'about:customizing';
-
-var locs = 'id,x,y,width,height\n';
-
-elements.forEach(i => {
-  let element = document.getElementById(i);
-  if (element) {
-    let bounds = element.getBoundingClientRect();
-    locs += i + ',' + bounds.x + ',' + bounds.y + ',' + bounds.width + ',' + bounds.height + '\n';
-  } else {
-    locs += i + ',-1,-1,0,0\n';
-  }
+    });
+    console.log(locs);
+  }, 1000);
+  gNavToolbox.removeEventListener("customizationready", UI_loaded);
 });
-console.log(locs);
+
+window.content.location = 'about:customizing';
