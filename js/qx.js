da2fd4404614b11ced01b809a04e054686dc68a3
Blake Winton <bwinton@latte.ca>
2015-01-29 11:09:11 -0500

    Add more statuses.  And links.

--- js/qx.js
+++ js/qx.js
@@ -15,19 +15,19 @@ globalstrict:true, nomen:false, newcap:false */
 (function () {
   var TRANSITION_DURATION = 500;
   var BUGZILLA_URL = 'https://api-dev.bugzilla.mozilla.org/latest/bug?' +
-      'include_fields=id,assigned_to,summary,last_change_time,whiteboard,status,resolution&' +
+      'include_fields=id,assigned_to,summary,last_change_time,whiteboard,status,mentor,resolution&' +
       'status=ALL&' +
       'whiteboard=\[qx\]';
 
   var sortFunc = function (a, b) {
-    var bug_statuses = this;
     var orderedStatuses = {
-      'unknown': 0,
-      'submitted': 1, 'posted': 1,
-      'assigned': 2, 'fixed': 2
+      // 'unknown': 0,
+      'not_ready': 1, 'needinfo': 1,
+      'submitted': 2, 'posted': 2,
+      'assigned': 3, 'fixed': 3
     };
-    var aStatus = bug_statuses[a.id];
-    var bStatus = bug_statuses[b.id];
+    var aStatus = a.qx_status;
+    var bStatus = b.qx_status;
 
     if (!aStatus) {
       if (!bStatus) {
@@ -39,8 +39,8 @@ globalstrict:true, nomen:false, newcap:false */
       return 1;
     }
 
-    aStatus = orderedStatuses[aStatus];
-    bStatus = orderedStatuses[bStatus];
+    aStatus = orderedStatuses[aStatus] || 0;
+    bStatus = orderedStatuses[bStatus] || 0;
     if (aStatus != bStatus) {
       return aStatus - bStatus;
     }
@@ -50,7 +50,8 @@ globalstrict:true, nomen:false, newcap:false */
 
   var getColour = function (status) {
     switch (status) {
-      case 'unknown':
+      case 'not_ready':
+      case 'needinfo':
         return 'rgb(255,255,255)';
       case 'submitted':
       case 'posted':
@@ -70,17 +71,53 @@ globalstrict:true, nomen:false, newcap:false */
     return 'nobody';
   }
 
-  var draw = function (all_bugs, bug_statuses) {
+  var getMentor = function (bug) {
+    if (bug.mentor && bug.mentor.content) {
+      return bug.mentor.content;
+    }
+    return false;
+  }
+
+  var getDxr = function (bug) {
+    if (bug.dxr !== 'no') {
+      return 'has link';
+    }
+    return false;
+  }
+
+  var getSpec = function (bug) {
+    if (bug.spec !== 'no') {
+      return 'has spec';
+    }
+    return false;
+  }
+
+  var draw = function (bugs) {
     d3.select('.bugs').select('.loading').remove()
 
     var bugRow = d3.select('.bugs').selectAll('.bug')
-      .data(all_bugs.sort(sortFunc.bind(bug_statuses)));
+      .data(bugs.sort(sortFunc));
 
     bugRow.enter().append('div').classed('bug', true)
-      .style({'opacity': '0', 'background-color': bug => getColour(bug_statuses[bug.id])})
-      .text(bug => bug.id + ' - ' + bug.summary + ' (' + getAssignee(bug) + ')')
-      .attr('title', bug => bug_statuses[bug.id])
-      .transition().duration(TRANSITION_DURATION)
+      .style({'opacity': '0', 'background-color': bug => getColour(bug.qx_status)})
+      .attr('title', bug => bug.qx_status);
+
+    bugRow.append('span').classed('icon glyphicon glyphicon-user', true)
+      .classed('missing', bug => !getMentor(bug))
+      .attr('title', bug => getMentor(bug) || 'nobody');
+    bugRow.append('span').classed('icon glyphicon glyphicon-search', true)
+      .classed('missing', bug => !getDxr(bug))
+      .attr('title', bug => getDxr(bug) || 'missing');
+    bugRow.append('span').classed('icon glyphicon glyphicon-picture', true)
+      .classed('missing', bug => !getSpec(bug))
+      .attr('title', bug => getSpec(bug) || 'missing');
+
+    bugRow.append('a').text(bug => bug.id)
+      .attr('href', bug => 'https://bugzilla.mozilla.org/show_bug.cgi?id=' + bug.id);
+    bugRow.append('span')
+      .text(bug => ' - ' + bug.summary + ' (' + getAssignee(bug) + ')');
+
+    bugRow.transition().duration(TRANSITION_DURATION)
       .delay((d, i) => i*20)
       .style({'opacity': '1'});
 
@@ -90,14 +127,15 @@ globalstrict:true, nomen:false, newcap:false */
       .remove();
   };
 
-  $.when(d3.csvPromise('data/qx.csv'), d3.jsonPromise(BUGZILLA_URL))
-    .then(function (bug_data, more_bug_data) {
-      var all_bugs = more_bug_data.bugs;
-      var bug_statuses = {};
-      $.each(bug_data, (i, bug) => {
-        bug_statuses[+bug.bugId] = bug.status;
+  $.when(d3.jsonPromise(BUGZILLA_URL), d3.csvPromise('data/qx.csv'))
+    .then(function (bug_list, bug_statuses) {
+      var bug_list = bug_list.bugs;
+      console.log(bug_list.slice());
+      $.each(bug_list, (i, bug) => {
+        $.extend(bug, bug_statuses.find(test => +test.id === +bug.id));
       });
-      draw(all_bugs, bug_statuses);
+      console.log(bug_list);
+      draw(bug_list);
     }).fail(function (error) {
       console.log('Fail', error);
     });
