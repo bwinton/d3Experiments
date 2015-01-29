c15d5466195e84e14427a482d754ce59abf25379
Blake Winton <bwinton@latte.ca>
2015-01-28 20:41:19 -0500

    Reindent for IIFE.

--- js/qx.js
+++ js/qx.js
@@ -13,62 +13,62 @@ globalstrict:true, nomen:false, newcap:false */
 'use strict';
 
 (function () {
-var TRANSITION_DURATION = 500;
-var BUGZILLA_URL = 'https://api-dev.bugzilla.mozilla.org/latest/bug?' +
-    'include_fields=id,assigned_to,summary,last_change_time,whiteboard,status,resolution&' +
-    'status=ALL&' +
-    'whiteboard=\[qx\]';
+  var TRANSITION_DURATION = 500;
+  var BUGZILLA_URL = 'https://api-dev.bugzilla.mozilla.org/latest/bug?' +
+      'include_fields=id,assigned_to,summary,last_change_time,whiteboard,status,resolution&' +
+      'status=ALL&' +
+      'whiteboard=\[qx\]';
 
 
-var getColour = function (status) {
-  switch (status) {
-    case 'unknown':
-      return 'rgb(255,255,255)';
-    case 'submitted':
-    case 'posted':
-      return 'rgb(127,255,127)';
-    case 'assigned':
-    case 'fixed':
-      return 'rgb(192,214,255)';
-    default:
-      return 'rgb(255,127,127)';
+  var getColour = function (status) {
+    switch (status) {
+      case 'unknown':
+        return 'rgb(255,255,255)';
+      case 'submitted':
+      case 'posted':
+        return 'rgb(127,255,127)';
+      case 'assigned':
+      case 'fixed':
+        return 'rgb(192,214,255)';
+      default:
+        return 'rgb(255,127,127)';
+    }
+  };
+
+  var getAssignee = function (bug) {
+    if (bug.assigned_to && bug.assigned_to.name) {
+      return bug.assigned_to.name;
+    }
+    return 'nobody';
   }
-};
 
-var getAssignee = function (bug) {
-  if (bug.assigned_to && bug.assigned_to.name) {
-    return bug.assigned_to.name;
-  }
-  return 'nobody';
-}
+  var draw = function (all_bugs, bug_statuses) {
+    var bugRow = d3.select('.bugs').selectAll('.bug')
+      .data(all_bugs);
 
-var draw = function (all_bugs, bug_statuses) {
-  var bugRow = d3.select('.bugs').selectAll('.bug')
-    .data(all_bugs);
+    bugRow.enter().append('div').classed('bug', true)
+      .style({'opacity': '0', 'background-color': bug => getColour(bug_statuses[bug.id])})
+      .text(bug => bug.id + ' - ' + bug.summary + ' (' + getAssignee(bug) + ')')
+      .attr('title', bug => bug_statuses[bug.id])
+      .transition().duration(TRANSITION_DURATION)
+      .delay((d, i) => i*20)
+      .style({'opacity': '1'});
 
-  bugRow.enter().append('div').classed('bug', true)
-    .style({'opacity': '0', 'background-color': bug => getColour(bug_statuses[bug.id])})
-    .text(bug => bug.id + ' - ' + bug.summary + ' (' + getAssignee(bug) + ')')
-    .attr('title', bug => bug_statuses[bug.id])
-    .transition().duration(TRANSITION_DURATION)
-    .delay((d, i) => i*20)
-    .style({'opacity': '1'});
+    bugRow.exit()
+      .transition().duration(TRANSITION_DURATION)
+      .style({'height': '0px', 'opacity': '0'})
+      .remove();
+  };
 
-  bugRow.exit()
-    .transition().duration(TRANSITION_DURATION)
-    .style({'height': '0px', 'opacity': '0'})
-    .remove();
-};
-
-$.when(d3.csvPromise('data/qx.csv'), d3.jsonPromise(BUGZILLA_URL))
-  .then(function (bug_data, more_bug_data) {
-    var all_bugs = more_bug_data.bugs;
-    var bug_statuses = {};
-    $.each(bug_data, (i, bug) => {
-      bug_statuses[+bug.bugId] = bug.status;
+  $.when(d3.csvPromise('data/qx.csv'), d3.jsonPromise(BUGZILLA_URL))
+    .then(function (bug_data, more_bug_data) {
+      var all_bugs = more_bug_data.bugs;
+      var bug_statuses = {};
+      $.each(bug_data, (i, bug) => {
+        bug_statuses[+bug.bugId] = bug.status;
+      });
+      draw(all_bugs, bug_statuses);
+    }).fail(function (error) {
+      console.log('Fail', error);
     });
-    draw(all_bugs, bug_statuses);
-  }).fail(function (error) {
-    console.log('Fail', error);
-  });
 })();
