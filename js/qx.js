c40dc2455fdc2a92c83aa5739ed75e3194af0810
Blake Winton <bwinton@latte.ca>
2015-11-10 10:09:35 -0500

    Add a priority sorter, and clean up the code.

--- js/qx.js
+++ js/qx.js
@@ -3,18 +3,22 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+/* eslint-env browser */
+/* global d3:false, $:false */
+/* eslint no-console:0 */
+
 'use strict';
 
 (function () {
   var TRANSITION_DURATION = 500;
   var WHITEBOARD = /\[qx(?::[^\]]*)*\]/;
   var BUGZILLA_URL = 'https://bugzilla.mozilla.org/rest/bug?' +
-      'include_fields=id,product,assigned_to,summary,last_change_time,whiteboard,status,mentors,resolution&' +
+      'include_fields=id,product,assigned_to,summary,last_change_time,whiteboard,status,mentors,resolution,priority&' +
       'status=ALL&' +
       'whiteboard=\[qx';
   var START_MOZILLA_URL = 'https://dl.dropboxusercontent.com/u/2301433/Twitter/startmozilla.tweets.txt';
 
-  var TEST_DATA = '/data/sample_bugzilla.json'
+  // var TEST_DATA = '/data/sample_bugzilla.json'
 
   // uncomment next line to use test data
   // BUGZILLA_URL = TEST_DATA
@@ -29,118 +33,150 @@
 
   var percent = d3.format('0.1%');
 
-  var sortFunc = function (a, b) {
-    var aStatus = a.qx_status;
-    var bStatus = b.qx_status;
-
-    if (!aStatus) {
-      if (!bStatus) {
-        return a.id - b.id
-      }
-      return -1;
-    }
-    if (!bStatus) {
-      return 1;
-    }
-
-    aStatus = orderedStatuses.get(aStatus) || 0;
-    bStatus = orderedStatuses.get(bStatus) || 0;
-    if (aStatus != bStatus) {
-      return aStatus - bStatus;
-    }
-
-    return a.id - b.id
-  }
-
   var products = [];
 
-  var getProductFromURL = function() {
+  var getProductFromURL = () => {
     var hash = window.location.hash;
 
     if(hash) {
-      return hash.replace('#', '');
+      return hash.replace('#', '').replace('<', '').replace('%3C', '');
     } else {
-      return false;
+      return '';
     }
-  }
+  };
   var currentProduct = getProductFromURL();
 
-  var filterFunc = function(d, i) {
+  var isURLSorted = () => {
+    var hash = window.location.hash;
+    return hash && (hash.indexOf('<') !== -1 || hash.indexOf('%3C') !== -1);
+  };
+
+  var getSortFromURL = () => {
+    if (isURLSorted()) {
+      return (a, b) => {
+        // console.log('Boom!', a, b);
+        console.log(a.id + ':' + a.priority, b.id + ':' + b.priority);
+        var aPrio = a.priority === '--' ? 'P9' : a.priority;
+        var bPrio = b.priority === '--' ? 'P9' : b.priority;
+        if (aPrio !== bPrio) {
+          console.log('    prio:', aPrio.localeCompare(bPrio));
+          return aPrio.localeCompare(bPrio);
+        }
+
+        console.log('    id:', a.id - b.id);
+        return a.id - b.id;
+      };
+    }
+
+    return (a, b) => {
+      var aStatus = a.qx_status;
+      var bStatus = b.qx_status;
+
+      if (!aStatus) {
+        if (!bStatus) {
+          return a.id - b.id;
+        }
+        return -1;
+      }
+      if (!bStatus) {
+        return 1;
+      }
+
+      aStatus = orderedStatuses.get(aStatus) || 0;
+      bStatus = orderedStatuses.get(bStatus) || 0;
+      if (aStatus != bStatus) {
+        return aStatus - bStatus;
+      }
+
+      return a.id - b.id;
+    };
+
+  };
+  var currentSort = getSortFromURL();
+
+  var filterFunc = (d) => {
     return currentProduct ? d.product.split(' ').join('-') == currentProduct : true;
-  }
+  };
 
-  var getColour = function (status) {
+  var getColour = (status) => {
     switch (status) {
-      case 'not_ready':
-        return 'rgb(255,255,255)';
-      case 'submitted':
-        return 'rgb(127,255,127)';
-      case 'assigned':
-      case 'fixed':
-        return 'rgb(192,214,255)';
-      default:
-        return 'rgb(255,127,127)';
+    case 'not_ready':
+      return 'rgb(255,255,255)';
+    case 'submitted':
+      return 'rgb(127,255,127)';
+    case 'assigned':
+    case 'fixed':
+      return 'rgb(192,214,255)';
+    default:
+      return 'rgb(255,127,127)';
     }
   };
 
-  var getClass = function (status) {
+  var getClass = (status) => {
     switch (status) {
-      case 'not_ready':
-        return '';
-      case 'submitted':
-        return 'success';
-      case 'assigned':
-      case 'fixed':
-        return 'info';
-      default:
-        return 'danger';
+    case 'not_ready':
+      return '';
+    case 'submitted':
+      return 'success';
+    case 'assigned':
+    case 'fixed':
+      return 'info';
+    default:
+      return 'danger';
     }
   };
 
-  var highlightLinks = function() {
+  var highlightLinks = () => {
     var links = $('.nav a');
     var current = window.location.hash;
     links.parent().removeClass('active');
-    links.each(function(){
+    links.each(() => {
       var hash = $(this).attr('href');
       if(hash == current) $(this).parent().addClass('active');
-    })
-  }
+    });
+  };
 
-  var getAssignee = function (bug) {
+  var getAssignee = (bug) => {
     if (bug.assigned_to) {
       return bug.assigned_to;
     }
     return 'nobody';
-  }
+  };
 
-  var getMentor = function (bug) {
+  var getMentor = (bug) => {
     if (bug.mentors && bug.mentors.length) {
       return bug.mentors.join(', ');
     }
     return false;
-  }
+  };
 
-  var getLink = function (bug) {
+  var getLink = (bug) => {
     if (bug.link && bug.link !== 'no') {
       return 'has link';
     }
     return false;
-  }
+  };
 
-  var getSpec = function (bug) {
+  var getSpec = (bug) => {
     if (bug.spec && bug.spec !== 'no') {
       return 'has spec';
     }
     return false;
-  }
+  };
 
-  var summarize = function (bugs) {
+  var getPriority = (bug) => {
+    if (bug.priority && bug.priority !== '--') {
+      return bug.priority;
+    }
+    return '';
+  };
+
+  var summarize = (bugs) => {
     var summary = [
       {name: ['unknown'], bugs: []},
       {name: ['not_ready'], bugs: []},
       {name: ['submitted'], bugs: []},
-      {name: ['assigned', 'fixed'], bugs: []},
+      {name: ['assigned', 'fixed'], bugs: []}
     ];
     bugs.forEach(bug => {
       var status = orderedStatuses.get(bug.qx_status) || 0;
@@ -152,15 +188,15 @@
       summary[status].percentage = summary[status].bugs.length / bugs.length;
     });
     return summary.filter(category => category.bugs.length);
-  }
+  };
 
-  var draw = function (bugs) {
+  var draw = (bugs) => {
 
     d3.select('.loading').remove();
     d3.select('.bugs').selectAll('.bug').remove();
 
     var bugRow = d3.select('.bugs').selectAll('.bug')
-      .data(bugs.filter(filterFunc).sort(sortFunc));
+      .data(bugs.filter(filterFunc).sort(currentSort));
 
     bugRow.enter().append('tr').attr('class', bug => getClass(bug.qx_status))
       .classed('bug', true)
@@ -169,7 +205,7 @@
       .attr('title', bug => bug.qx_status);
 
     //product
-    bugRow.append('td').classed('product', true).text(bug => bug.product)
+    bugRow.append('td').classed('product', true).text(bug => bug.product);
 
     //icons
     //mentor
@@ -191,7 +227,10 @@
     bugRow.append('td').append('a').text(bug => bug.id).classed('bugid', true)
       .attr('href', bug => 'https://bugzilla.mozilla.org/show_bug.cgi?id=' + bug.id);
 
-    var summary = bugRow.append('td')
+    bugRow.append('td').append('span')
+      .text(bug => getPriority(bug)).classed('priority', true);
+
+    var summary = bugRow.append('td');
     summary.append('span').classed('assignee', true)
       .text(bug => getAssignee(bug));
     summary.append('span').classed('summary', true)
@@ -225,23 +264,26 @@
       .text(name => name).classed('fixed', name => name === 'fixed');
     category.append('span').text(category => ': ' + category.bugs.length);
     category.append('span').text(category => ' (' + percent(category.percentage) + ')');
+
+    var priority = $('#priority');
+    priority.attr('href', '#' + getProductFromURL() + (isURLSorted() ? '' : '<'));
   };
 
   $.when(d3.jsonPromise(BUGZILLA_URL), d3.htmlPromise(START_MOZILLA_URL))
-    .then(function (bug_list, startmozilla) {
-      var bug_list = bug_list.bugs;
-      bug_list.forEach(function(bug){
+    .then((bug_list, startmozilla) => {
+      bug_list = bug_list.bugs;
+      bug_list.forEach((bug) => {
         var product = bug.product.split(' ').join('-');
         if (products.indexOf(product) == -1) products.push(product);
       });
 
-      var list = $('.nav');
+      var list = $('ul.nav');
       var li = $('<li/>').appendTo(list);
-      $('<a/>').attr('href', '#').text("All").appendTo(li);
-      $.each(products, function(i, product){
+      $('<a/>').attr('href', '#').text('All').appendTo(li);
+      $.each(products, (i, product) => {
         var li = $('<li/>').appendTo(list);
         $('<a/>').attr('href', '#' + product).text(product.split('-').join(' ')).appendTo(li);
-      })
+      });
 
       var posts = [];
 
@@ -258,11 +300,11 @@
       }
 
       bug_list.forEach(bug => {
-        var whiteboard = WHITEBOARD.exec(bug.whiteboard)[0].slice(4, -1).split(":");
+        var whiteboard = WHITEBOARD.exec(bug.whiteboard)[0].slice(4, -1).split(':');
         whiteboard.forEach(key => {
           if (key) {
             if (key !== 'link' && key !== 'spec') {
-              console.log("!!!!!!!", bug.id, key);
+              console.log('!!!!!!!', bug.id, key);
             }
             bug[key] = true;
           }
@@ -284,12 +326,13 @@
       });
       draw(bug_list);
       highlightLinks();
-      window.onhashchange = function(){
+      window.onhashchange = () => {
         currentProduct = getProductFromURL();
+        currentSort = getSortFromURL();
         draw(bug_list);
         highlightLinks();
-      }
-    }).fail(function (error) {
+      };
+    }).fail((error) => {
       console.log('Fail', error);
     });
 })();
